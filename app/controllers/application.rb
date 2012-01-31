9a901fdc10f1eade2983087b360c55ba658bfef8
Gun.io Whitespace Robot <contact@gun.io>
2012-01-31 12:55:39 -0500

    Remove whitespace [WhitespaceBot]

--- app/controllers/application.rb
+++ app/controllers/application.rb
@@ -7,9 +7,9 @@ class ApplicationController < ActionController::Base
   # See ActionController::RequestForgeryProtection for details
   # Uncomment the :secret if you're not using the cookie session store
   protect_from_forgery # :secret => 'cfaf6fd9f0c206d26baad189a1a8f448'
-  
-  # See ActionController::Base for details 
+
+  # See ActionController::Base for details
   # Uncomment this to filter the contents of submitted sensitive data parameters
-  # from your application log (in this case, all fields with names like "password"). 
+  # from your application log (in this case, all fields with names like "password").
   # filter_parameter_logging :password
 end
